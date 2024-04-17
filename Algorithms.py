Fix minor bugs in Gradient Descent implementation

Algorithms.py
@@ -17,62 +17,65 @@ class GradDescLR:
         self.iter = iter
 
     # Note that input below can be any training data or unseen (test) data
-    def Predict(self,input:np.array, parameters:np.array, intersection:float) -> np.array:
-        if len(parameters)!= self.obs:
-            raise Exception (f"parameters={parameters} have the wrong size")
-        
-        if parameters.shape[1] != input.shape[0]:
-            raise Exception (f"Wrong parameter <=> input shape, {parameters.shape[1]}!={input.shape[0]}")
+    def Predict(self,input:np.array, parameters:np.array) -> np.array:
+        n_params = len(parameters)
 
-        if intersection.shape[0]!=input.shape[0]:
-            raise Exception (f"Wrong intersection <=> input shape, {intersection.shape[0]}!={input.shape[0]}")
+        if n_params != self.feat:
+            raise Exception (f"parameters={parameters} have the wrong size")
+                
+        if n_params != input.shape[1]:
+            raise Exception (f"Wrong parameter <=> input shape, {n_params}(n_params)!={input.shape[1]}(input_features)")
 
         # This is an array, where each column represents the i-th prediction
         # for input in x
-        return np.dot(parameters,input)+intersection
+        return np.dot(input,parameters)
     
-    def _Cost(self, parameters:np.array, intersection:float) -> float:
+    def _Cost(self, parameters:np.array) -> float:
         
-        predictions = self.Predict(self.x, parameters,intersection)
+        # Prediction seems to be okay
+        predictions = self.Predict(self.x, parameters)
         
         if predictions.shape!=self.y.shape:
             raise Exception (f"targets.shape({self.y.shape})!=predictions.shape({predictions.shape})")
-
-        return (0.5/self.obs)*np.sum((predictions-self.y)**2)
-
-    def _Derivatives(self, parameters, intersection) -> Tuple[np.array,float]:
         
-        predictions = self.Predict(self.x, parameters, intersection)
-        errors = (predictions-self.y)**2
+        errors_sq = (predictions-self.y)**2
 
-        param_der = (1/self.obs)*np.dot(errors,self.x.T)
-        b_der = (1/self.obs)*np.sum(errors)
+        return (0.5/self.obs)*np.sum(errors_sq)
+
+    def _Derivatives(self, parameters) -> Tuple[np.array,float]:
+        
+        predictions = self.Predict(self.x, parameters)
+        errors = predictions-self.y
+
+        # Check that first component of self.x.T is a column with only ones
+        param_der = (1/self.obs)*np.dot(self.x.T,errors)
 
         # Check derivatives has the correct dimensions
-        if param_der.shape!=(self.feat,1):
+        if param_der.shape!=(self.feat,):
             raise Exception ("Something went wrong, derivatives has incorrect size!")
         
-        return param_der,b_der
+        return param_der
 
-    def GradDesc(self,alpha:float) -> Tuple[np.array, float, np.array]:
+    def Compute_Coefficients(self) -> Tuple[np.array, list]:
         
-        alphas = [0.001,0.003,0.01,0.03,0.1,0.3,1.0,3.0,10.0,30.0][::-1]
+        alphas = [0.0001,0.0003,0.01,0.03,0.1,0.3,1.0,3.0,10.0,30.0][::-1]
 
         for alpha in alphas:
+            # Start gradient descent with generic values (1s for instance)
             w = np.ones(self.feat)
-            b = 1.0
             k = alpha/self.obs
-            prev_cost = self._Cost(w,b)
+            prev_cost = self._Cost(w) # Cost is correct
             finalized=True
-            log_costs = np.array([])
+            log_costs = list()
+            #print(f"computing gradient descent for alpha={alpha}")
             for i in range(self.iter+1):
-                # Start gradient descent with generic values (1s for instance)
-                w_dev,b_dev = self._derivatives(w,b)
+                
+                # Compute gradient and update parameters
+                w_dev = self._Derivatives(w)
                 w -= k*w_dev
-                b -= k*b_dev
 
                 # Compute cost for each w and b
-                cost = self._Cost(w,b)
+                cost = self._Cost(w)
 
                 if cost>prev_cost:
                     warnings.warn("Cost is increasing, alpha is too big")
@@ -80,14 +83,13 @@ class GradDescLR:
                     break
                 prev_cost = cost
                 log_costs.append(cost)
-                print(f"Cost={cost}, iteration={i}")
             
             # Since we are starting with the highest alpha, the first alpha
             # that makes gradient descent converge is the most optimal one,
             # so we don't need to iterate over the rest
             if finalized:
-                print("Gradient Descent has finished")
-                return (w,b,log_costs)
+                print(f"Gradient Descent has finished for alpha={alpha}")
+                return (w,log_costs)
         # if we get here is because none of the alphas made gradient descent to converge
         # in which case raise a program error
         raise ValueError(f"None of the alphas={self.alphas} makes Gradient Descent converge, check")