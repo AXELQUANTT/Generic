3cc42f06486d06219649572caba6289871e3b09c
Axel Borasino Marques <boma.axel@gmail.com>
2024-05-26 23:45:59 +0200

    Big update to analysis.py file
    - Add bunch of to-do lists.
    - Enable package to write orderbooks into a csv file.

--- AlgoTrading/orderbook/analysis.py
+++ AlgoTrading/orderbook/analysis.py
@@ -6,14 +6,52 @@ single updates and create some alphas
 from the order book
 """
 
+import glob
+import csv
+from utils import orderbook,generate_header
+import sys, importlib
+importlib.reload(sys.modules['utils'])
+import time
 
 
+# TO-DO_1: Use command line options to run the script
+# TO_DO_2: Re-organize/refactorize code in 
+#          generate_orderbooks function
+# TO_DO_3: Deal with in/out files in a better way. Probably
+#          better to create an output folder storing the
+#          ob files
 
+def generate_orderbooks(path:str) -> None:
+    files = glob.glob(path)
+    k = 0
+    print("starting construction of orderbooks...")
+    for file in files:
+        # define all file specific variables
+        ob = orderbook()
+        # Create the corresponding output file
+        full_path = file.split('/')
+        out_file = f"{'/'.join(full_path[:-1])}/out_{full_path[-1].split('_')[-1]}"
+        with open(file,"r") as rf:
+            reader = csv.reader(rf,delimiter=',')
+            # skip header
+            next(reader,None)
+            with open(out_file,"w") as wf:
+                writer = csv.writer(wf,delimiter=',')
+                # write the header to the output csv file
+                writer.writerow(generate_header())
+                for _,line in enumerate(reader):
+                    ob.process_update(line)
+                    ob.generate_ob_view()
+                    line_to_write = ob._format_output()
+                    writer.writerow(line_to_write)
+        k += 1
+        print(f"Progress...{k/len(files)}")
 
-
-
-
-
+start_time = time.time()
+path = "/home/axelbm23/Code/AlgoTrading/orderbook/codetest/res_*.csv"
+generate_orderbooks(path)
+end_time = time.time()
+print(f"Total execution time(s)={end_time-start_time}")
 
 # Part 2)
 # 2.1 Come up with a set of statistics that according to research
