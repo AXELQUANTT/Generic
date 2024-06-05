f68072441061a52ef0aed9ef60885c9b4561bec1
Axel Borasino Marques <boma.axel@gmail.com>
2024-06-05 11:38:03 +0200

    Add generic parameters optimizer function and test

--- ML_AI/Algos/Utils.py
+++ ML_AI/Algos/Utils.py
@@ -3,7 +3,7 @@ Package with some utils that may be quite handy
 for general ML_AI projects
 """
 
-
+import keras
 import tensorflow as tf
 import numpy as np
 
@@ -29,3 +29,26 @@ def compute_grad_desc(tf_variables:tf.Variable,
         tf_variables.assign_add(-alpha * derivates)
     
     return tf_variables
+
+
+# We can also think of the same concept but for a generic
+# optimizer
+def optimize_params(tf_variables:tf.Variable,
+                    x:np.array,
+                    y:np.array,
+                    cost_tf_vars,
+                    optimizer:keras.Optimizer,
+                    n_iters:int=100):
+    
+    for _ in range(n_iters):
+        with tf.GradientTape() as tape:
+            costJ = cost_tf_vars(tf_variables,x,y)
+        
+        derivates = tape.gradient(costJ, [tf_variables])
+
+        optimizer.apply_gradients(zip(derivates,[tf_variables]))
+    
+    return tf_variables
+    
+# TO-DO: Encapsulate these two in compute_parameters class
+# and put a flag to select which optimized we want
--- ML_AI/Algos/Utils_tests.py
+++ ML_AI/Algos/Utils_tests.py
@@ -2,6 +2,7 @@
 Unit test for utils package
 """
 
+import keras
 import numpy as np
 import tensorflow as tf
 import Utils
@@ -36,6 +37,25 @@ w_optimized = np.array(Utils.compute_grad_desc(tf_variables=w,x=x,y=y,n_iters=it
 w_theo = np.array([1.0])
 
 tests.append(Test(w_optimized,w_theo,0.001))
+
+################
+# Adam optimizer
+################
+
+w = tf.Variable(3.0)
+x = [1.0]
+y = [1.0]
+iters = 1000
+adam = keras.optimizers.Adam(learning_rate=0.1)
+def costJ(w,x,y):
+    return (w*x-y)**2
+
+adam_optimized = np.array(Utils.optimize_params
+                       (tf_variables=w,x=x,y=y,cost_tf_vars=costJ,
+                        optimizer=adam,n_iters=iters))
+adam_theo = np.array([1.0])
+
+tests.append(Test(adam_optimized,adam_theo,0.001))
 
 #####################################################################
 ############################### CHECK ############################### 
