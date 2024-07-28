608a3c7bebf1e658b1205dfd0841c5594ff2ba88
Axel Borasino Marques <boma.axel@gmail.com>
2024-07-29 00:23:58 +0200

    Fix bugs when importing data, add regression analysis

--- ML_AI/Projects/CRM/crm.py
+++ ML_AI/Projects/CRM/crm.py
@@ -5,6 +5,7 @@ import pandas as pd
 import re
 import sqlite3
 from sqlite3 import Cursor, OperationalError
+from sklearn import linear_model
 from typing import Any, Tuple, Optional
 
 class sql_data():
@@ -95,14 +96,13 @@ class sql_data():
             f'{table}.age, {table}.pay_status, '\
             f'{table}.home_ownership, '\
             f'{table}.verification_status, '\
+            f'{table}.purpose, '\
+            f'{table}.addr_state, '\
             f'api_subgrade.id AS sub_grade_id, '\
             f'api_homeownership.id AS home_ownership_id, '\
-            f'api_verificationstatus.id AS verification_status_id, '\
-            f'api_purpose.id AS purpose_id, api_state.id AS addr_state_id '\
+            f'api_verificationstatus.id AS verification_status_id '\
             f'FROM {table} LEFT JOIN api_homeownership '\
             f'ON {table}.home_ownership{org_suff}=api_homeownership.name LEFT JOIN '\
-            f'api_purpose ON {table}.purpose{org_suff}=api_purpose.name LEFT JOIN '\
-            f'api_state ON {table}.addr_state{org_suff}=api_state.name LEFT JOIN '\
             f'api_subgrade ON {table}.sub_grade{org_suff}=api_subgrade.name LEFT JOIN '\
             f'api_verificationstatus ON {table}.verification_status{org_suff}='\
             f'api_verificationstatus.name;'
@@ -132,13 +132,60 @@ class sql_data():
             f'api_verificationstatus.{targ_suff};'
         
         return query
-
+    
+    def _add_faulty_data(self, df:pd.DataFrame, table:str, col:str):
+        if table=='api_newcustomer':
+            return df
+        
+        if col=='purpose':
+            # TO-DO: Instead of a dict, use a library to create the plural
+            # and then just do upper case
+            correct_labels = {'debt_consolidation':'DEBT_CONSOLIDATIONS',
+                              'other':'OTHER', 
+                              'credit_card':'CREDIT_CARDS', 
+                              'major_purchase':'MAJOR_PURCHASE',
+                              'medical':'MEDICAL',
+                              'car':'CARS', 
+                              'home_improvement':'HOME_IMPROVEMENTS',
+                              'small_business':'SMALL_BUSINESS',
+                              'house':'HOUSES',
+                              'wedding':'WEDDINGS',
+                              'vacation':'VACATIONS',
+                              'moving':'MOVING',
+                              'renewable_energy':'RENEWABLE_ENERGY',
+                              'educational':'EDUCATIONAL'}
+            df['purpose'] = df['purpose'].apply(lambda x: correct_labels[x])
+            api_purpose = self.create_df("SELECT name as purpose, id as purpose_id FROM api_purpose;")
+            df = pd.merge(df, api_purpose, left_on='purpose', right_on='purpose', how='left')
+            # Finally remove the purpose column as we do not need it
+            df.drop(['purpose'], axis=1, inplace=True)
+            return df
+        
+        if col=='addr_state':
+            # TO-DO: Add USA suffix to that col and merge
+            df['addr_state'] = df['addr_state'].apply(lambda x: f'USA_{x.upper()}')
+            api_state = self.create_df("SELECT name as addr_state, id as addr_state_id from api_state;")
+            df = pd.merge(df, api_state, left_on='addr_state', right_on='addr_state', how='left')
+            # Finally remove the addr_state column as we do not need it
+            df.drop(['addr_state'], axis=1, inplace=True)
+            return df
+        
     def import_all_data(self, table:str) -> pd.DataFrame:
         query = self._query_for_all_data(table)
         df = self.create_df(query)
 
-        
-        self._query_additional_data(table)
+        # Upon inspection I've noticed that there are two
+        # columns inside oldcustomer data
+        # that do not match the values in their respective tables.
+        # One is api_oldcustomer.purpose, whose values
+        # are in lower case and do not exactly match the values
+        # in api_purpose.name. Some of them are in singular, some
+        # of them in plural, so need standarization.
+
+        # The second ones are api_oldcustomer.addr_state, which
+        # do not have the USA suffix present in api_state.
+        df = self._add_faulty_data(df, table, 'purpose')
+        df = self._add_faulty_data(df, table, 'addr_state')
 
         # Make sure we don't have duplicated columns
         if any(df.columns.duplicated()):
@@ -150,6 +197,8 @@ class sql_data():
             raise ValueError(f'table = {table} is missing the following fields => '\
                              f'{",".join([str(x) for x in set(wanted_fields)-set(df.columns)])}')
 
+        # TO-DO: Make sure all the data from external columns
+        # has the correct values
         return df
     
 # Create dummies for homeownership and verification_status
@@ -206,9 +255,26 @@ def fix_wrong_units(df:pd.DataFrame, want_fields:dict, fix_qties:dict) -> pd.Dat
                 print(f'col={col} with type {des_type} could not be transformed, check')
     return df
 
+def run_lasso_regression(df:pd.DataFrame,
+                         predictors:list[str],
+                         target:str,
+                         alpha:float=1.0) -> tuple[list[float],list[float],float,linear_model.Lasso]:
+    """
+    Functiond devoted to compute Lasso linear
+    regression. 
+    """
+
+    lasso = linear_model.Lasso(alpha=alpha,fit_intercept=False)
+    lasso.fit(X=df[predictors], y=df[target])
+    coefs = lasso.coef_
+    intercept = lasso.intercept_
+    score = lasso.score(X=df[predictors], y=df[target])
+
+    return coefs,intercept,score,lasso
+
 def fix_nans(df:pd.DataFrame) -> pd.DataFrame:
-    pass
     # We will treat Nans a bit differently depending on their source
+    pass
 
     # For the case of Nans in employment length, the most straightforward
     # relationship is to link it with the age of the borrower. In order
@@ -219,6 +285,20 @@ def fix_nans(df:pd.DataFrame) -> pd.DataFrame:
     # longer employment contracts
     
     #fix_emp_length
+    # for the emp_length we will try with age.
+    emp_len_by_age = df.groupby(['age']).agg({'emp_length':'median'})
+    emp_len_by_ = df.groupby(['emp_length']).agg({'age':'median'})
+
+    # Checking with demographic features does not seem to provide any
+    # meaningful insight.
+
+    # Check with credit features. For instance, 
+    df['interest_rate']
+
+
+    # We also expect higher employment contract lengths to have
+    # more favorable interest conditions
+
 
     # For the fico_range_high, the first constraint we have
     # to pass is that its value >= fico_range_low. FICO values
@@ -293,21 +373,20 @@ for table in all_tables:
 
 
 # Upon inspection, tables containing most of the data are newcustomer and oldcustomer 
-desired_tables = ['api_newcustomer', 'api_oldcustomer']
+desired_tables = ['api_oldcustomer', 'api_newcustomer']
 dfs = []
 oldcustomer = db.create_df("SELECT * from api_oldcustomer;")
 newcustomer = db.create_df("SELECT * from api_newcustomer;")
-# TO-DO:
+
 # So for the oldcustomer table, there are some case labels that
 # do not match exactly the ones in purpose table. Format them
 # so that they match
 purpose = db.create_df("SELECT * from api_purpose;")
 
-# TO-DO: api_state values start with USA, whereas
+# api_state values start with USA, whereas
 # the values in oldcustomer database do not start with that
 # suffix. 
 addr_state = db.create_df("SELECT * from api_state;")
-
 
 for table in desired_tables:
 
@@ -325,20 +404,28 @@ for table in desired_tables:
     # 4. annual_inc has a range for some observations instead
     # of a single value (e.g (72000.0, 80000.0]) 
     # For those cases, compute the mean.
-
-    # Fix wrong quantities and assing proper format to columns
-    df = fix_wrong_units(df, wanted_fields, fix_qties)
     # Auxiliary column to debug
     df['table'] = table
     dfs.append(df)
-
+    
 df = pd.concat(dfs)
 
-# Perform NaN treatment
-cols_with_nans = [col for col in df.columns if df[col].isnull().any()]
-print(f'The following columns have NaNs, {",".join(cols_with_nans)}')
+# Fix wrong quantities and assign proper format to columns
+df = fix_wrong_units(df, wanted_fields, fix_qties)
+
+# Compute percentage of NaN values for each column
+nans_per_col = df.isnull().sum()*100.0/len(df)
+
+# For the number of public_rec_bankruptcies and
+# revol_util, the numbers are below the 0.05%, a number
+# of observations so small that we assume no statistical
+# difference will be imposed by removing these faulty records
+
+# One try will to impose min_max normalization in the data
+# and try to predict the missing values with the rest
 
 
+#print(df)
 
 # After the NaN treatment, no NaNs should be present, assert it
 #if any(df.isnull()):
