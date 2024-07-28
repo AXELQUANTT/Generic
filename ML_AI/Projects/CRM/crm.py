1f3b17cd7cf0d0873acbb7d7f0e765885e98bb4c
Axel Borasino Marques <boma.axel@gmail.com>
2024-07-28 20:32:10 +0200

    Beta version of crm project

--- ML_AI/Projects/CRM/crm.py
+++ ML_AI/Projects/CRM/crm.py
@@ -1,6 +1,8 @@
-
+import dateparser
 import datetime
+import os
 import pandas as pd
+import re
 import sqlite3
 from sqlite3 import Cursor, OperationalError
 from typing import Any, Tuple, Optional
@@ -40,7 +42,7 @@ class sql_data():
         """
         Retrieves all table names in the db
         """
-        tables,_ = self._query_db("SELECT name FROM sqlite_master")
+        tables,_ = self._query_db("SELECT name FROM sqlite_master;")
 
         # ignore sqlite_sequence, which only contains info about the autoincrement
         return [table[0] for table in tables if table[0]!='sqlite_sequence']
@@ -72,49 +74,214 @@ class sql_data():
         """
         Creates query to get all the relevant information
         """ 
-        match table:
-            case 'api_newcustomer':
-                org_suff = '_id'
-                targ_suff = 'id'
-            case 'api_oldcustomer':
-                org_suff = ''
-                targ_suff = 'name'
-            case _:
-                raise ValueError(f'table has to be one of [api_newcustomer, api_oldcustomer] '
-                                 f', {table} provided instead')
-            
-        query = f'SELECT {table}.*, '\
-        f"api_homeownership.name AS api_homeownership_name, "\
-        f"api_homeownership.id AS api_homeownership_id, "\
-        f"api_purpose.name AS api_purpose_name, "\
-        f"api_purpose.id AS api_purpose_id, "\
-        f"api_state.name AS api_state_name, "\
-        f"api_state.id AS api_state_id, "\
-        f"api_subgrade.name AS api_subgrade_name, "\
-        f"api_subgrade.id AS api_subgrade_id, "\
-        f"api_verificationstatus.id AS api_verificationstatus_id, "\
-        f"api_verificationstatus.name AS api_verificationstatus_name "\
-        f"FROM {table} LEFT JOIN api_homeownership "\
-        f"ON {table}.home_ownership{org_suff}=api_homeownership.{targ_suff} LEFT JOIN "\
-        f"api_purpose ON {table}.purpose{org_suff}=api_purpose.{targ_suff} LEFT JOIN "\
-        f"api_state ON {table}.addr_state{org_suff}=api_state.{targ_suff} LEFT JOIN "\
-        f"api_subgrade ON {table}.sub_grade{org_suff}=api_subgrade.{targ_suff} LEFT JOIN "\
-        f"api_verificationstatus ON {table}.verification_status{org_suff}="\
-        f"api_verificationstatus.{targ_suff};"
-
+        
+        # This query is where most of the standarization of the columns happens
+        # In the case of the api_newcustomer, all columns on it are the ids, so
+        # we only need to retrieve the names from the other ones
+        # In the case of the oldcustomer, all columns corresponds to the names,
+        # so we only need to retrieve the ids from the other ones
+        if table=='api_oldcustomer':
+            org_suff = ''
+            targ_suff = 'name'
+            query = f'SELECT {table}.loan_status, '\
+            f'{table}.loan_amnt, {table}.term, '\
+            f'{table}.int_rate, {table}.installment, '\
+            f'{table}.emp_length, {table}.annual_inc, '\
+            f'{table}.dti, {table}.issue_d, '\
+            f'{table}.fico_range_low, {table}.fico_range_high, '\
+            f'{table}.open_acc, {table}.pub_rec, '\
+            f'{table}.revol_bal, {table}.revol_util, '\
+            f'{table}.mort_acc, {table}.pub_rec_bankruptcies, '\
+            f'{table}.age, {table}.pay_status, '\
+            f'{table}.home_ownership, '\
+            f'{table}.verification_status, '\
+            f'api_subgrade.id AS sub_grade_id, '\
+            f'api_homeownership.id AS home_ownership_id, '\
+            f'api_verificationstatus.id AS verification_status_id, '\
+            f'api_purpose.id AS purpose_id, api_state.id AS addr_state_id '\
+            f'FROM {table} LEFT JOIN api_homeownership '\
+            f'ON {table}.home_ownership{org_suff}=api_homeownership.name LEFT JOIN '\
+            f'api_purpose ON {table}.purpose{org_suff}=api_purpose.name LEFT JOIN '\
+            f'api_state ON {table}.addr_state{org_suff}=api_state.name LEFT JOIN '\
+            f'api_subgrade ON {table}.sub_grade{org_suff}=api_subgrade.name LEFT JOIN '\
+            f'api_verificationstatus ON {table}.verification_status{org_suff}='\
+            f'api_verificationstatus.name;'
+        else:
+            org_suff = '_id'
+            targ_suff = 'id'
+            query = f'SELECT {table}.loan_status, '\
+            f'{table}.loan_amnt, {table}.term, '\
+            f'{table}.int_rate, {table}.installment, '\
+            f'{table}.employment_length AS emp_length, '\
+            f'{table}.annual_inc, '\
+            f'{table}.issued as issue_d, {table}.dti, '\
+            f'{table}.fico_range_low, {table}.fico_range_high, '\
+            f'{table}.open_acc, {table}.pub_rec, '\
+            f'{table}.revol_bal, {table}.revol_util, '\
+            f'{table}.mort_acc, {table}.pub_rec_bankruptcies, '\
+            f'{table}.age, {table}.payment_status AS pay_status, '\
+            f'{table}.home_ownership_id, '\
+            f'{table}.purpose_id, {table}.sub_grade_id, '\
+            f'{table}.verification_status_id, '\
+            f'{table}.addr_state_id, '\
+            f'api_homeownership.name AS home_ownership, '\
+            f'api_verificationstatus.name AS verification_status '\
+            f'FROM {table} LEFT JOIN api_homeownership '\
+            f'ON {table}.home_ownership{org_suff}=api_homeownership.{targ_suff} LEFT JOIN '\
+            f'api_verificationstatus ON {table}.verification_status{org_suff}='\
+            f'api_verificationstatus.{targ_suff};'
+        
         return query
 
     def import_all_data(self, table:str) -> pd.DataFrame:
         query = self._query_for_all_data(table)
         df = self.create_df(query)
-        return df
 
         
+        self._query_additional_data(table)
+
+        # Make sure we don't have duplicated columns
+        if any(df.columns.duplicated()):
+            raise ValueError(f'dataframe from table {table} contains duplicated columns, '\
+                             f'{",".join(df.columns[df.columns.duplicated()])}')
+        
+        # If all data has been correctly retrieve, we should have 26 columns, assert that
+        if len(df.columns)!=len(wanted_fields.keys()):
+            raise ValueError(f'table = {table} is missing the following fields => '\
+                             f'{",".join([str(x) for x in set(wanted_fields)-set(df.columns)])}')
+
+        return df
+    
+# Create dummies for homeownership and verification_status
+def create_dummies(df:pd.DataFrame, col:str) -> pd.DataFrame:
+    # For each of different values in col, create a dummy column
+    dummies = pd.get_dummies(df[col])
+    # Format the columns according to the table 1
+    dummies.columns = [f'is_{col.lower().replace(" ","_")}' for col in dummies.columns]
+    df = df.join(dummies)
+    return df
+
+def is_tuple(value) -> bool:
+    if isinstance(value, str):
+        return '(' in value or ')' in value or '[' in value or ']' in value
+    return False
+
+def mean_from_tuple(value) -> float:
+    value = re.sub(r"[\([{})\]]", "", value)
+    return 0.5*sum([float(x) for x in value.split(', ')])
+
+def fix_wrong_units(df:pd.DataFrame, want_fields:dict, fix_qties:dict) -> pd.DataFrame:
+    """
+    Function devoted to fix the values of those quantities that are wrong
+    """
+    # TO-DO: Clean this logic, quite convoluted
+    for col in want_fields.keys():
+        des_type = want_fields[col]
+        if des_type==datetime.datetime:
+            try:
+                df[col] = df[col].apply(lambda x: dateparser.parse(x))
+            except ValueError as e:
+                print(f'col={col} with type datetime could not be transformed, check')
+
+        elif col=='annual_inc':
+            try:
+                df[col] = df[col].apply(lambda x: x if pd.isnull(x) else des_type(mean_from_tuple(x)) if is_tuple(x) else des_type(x))
+            except ValueError as e:
+                print(f'col={col} could not be transformed, check')
+
+        elif col in fix_qties:
+            error = fix_qties[col][0]
+            multiplier = fix_qties[col][1]
+            try:
+                df[col] = df[col].apply(lambda x: x if pd.isnull(x) else des_type(float(x.lower()[:-1])*multiplier)
+                                         if (isinstance(x, str) and x.lower().endswith(error)) else des_type(x))
+                
+            except ValueError as e:
+                print(f'col={col} with type {des_type} could not be transformed, check')
+
+        else:
+            try:
+                df[col] = df[col].apply(lambda x: x if pd.isnull(x) else des_type(x))
+            except ValueError as e:
+                print(f'col={col} with type {des_type} could not be transformed, check')
+    return df
+
+def fix_nans(df:pd.DataFrame) -> pd.DataFrame:
+    pass
+    # We will treat Nans a bit differently depending on their source
+
+    # For the case of Nans in employment length, the most straightforward
+    # relationship is to link it with the age of the borrower. In order
+    # to do so, we can run an ordinary least square regression of the
+    # employment length wrt the age of the borrower. Another approach
+    # is to link with the reported annual income provided by the user
+    # , as we expect workers with longer annual income to have
+    # longer employment contracts
+    
+    #fix_emp_length
+
+    # For the fico_range_high, the first constraint we have
+    # to pass is that its value >= fico_range_low. FICO values
+    # are an indicator representing the credit quality of the
+    # borrower. The main factors determining the value of a FICO
+    # score are the payment history,the current level of indebtedness,
+    # the type of credit used, the length of the credit history 
+    # and new credit accounts.
+    
+    #fix_fico_score
+
+
+    # revol_util is the amount of credit the borrower is using relative
+    # to all available revolving credit
+    
+    #fix_revol_util
+
+    # mort_acc, is the number of mortatge_accounts. Note we have
+    # open_acc, which is the total number of open credit lines
+    # by the borrower, which sets an upper limit for this amount
+    
+    #fix_mort_acc
+
+    # pub_rec_bankruptcies, is the number of public 
+    # record bankruptcies. We have pub_rec, which is the
+    # number of derogatory public records, which can be
+    # used as an upper limit for this qty
+    
+    #fix_pub_rec_bankruptcies
 
 path = "/home/axelbm23/Code/ML_AI/Projects/CRM/customer_data.sqlite3"
-wrong_labels = {'employment_length':'emp_length',
-                'issued':'issue_d',
-                'payment_status':'pay_status'}
+
+# TO-DO: Read wrong quantities from file
+fix_qties = {'loan_amnt':('k',1_000),
+               'term':('y',12)}
+
+wanted_fields = {'loan_status':bool,
+                 'loan_amnt':int,
+                 'term':int,
+                 'int_rate':float,
+                 'installment':float,
+                 'sub_grade_id':int, # need to remove the id at the end
+                 'emp_length':int,
+                 'home_ownership':str,
+                 'home_ownership_id':int, # need to remove the id at the end
+                 'annual_inc':int,
+                 'verification_status':str,
+                 'verification_status_id':int, # need to remove the id at the end
+                 'issue_d':datetime.datetime,
+                 'purpose_id':int, # need to remove the id at the end
+                 'addr_state_id':int, # need to remove the id at the end
+                 'dti':float,
+                 'fico_range_low':int,
+                 'fico_range_high':int,
+                 'open_acc':int,
+                 'pub_rec':int,
+                 'revol_bal':int,
+                 'revol_util':float,
+                 'mort_acc':int,
+                 'pub_rec_bankruptcies':int,
+                 'age':int,
+                 'pay_status':int}
+
 
 db = sql_data(path)
 all_tables = db.get_all_tables()
@@ -127,28 +294,63 @@ for table in all_tables:
 
 # Upon inspection, tables containing most of the data are newcustomer and oldcustomer 
 desired_tables = ['api_newcustomer', 'api_oldcustomer']
+dfs = []
+oldcustomer = db.create_df("SELECT * from api_oldcustomer;")
+newcustomer = db.create_df("SELECT * from api_newcustomer;")
+# TO-DO:
+# So for the oldcustomer table, there are some case labels that
+# do not match exactly the ones in purpose table. Format them
+# so that they match
+purpose = db.create_df("SELECT * from api_purpose;")
+
+# TO-DO: api_state values start with USA, whereas
+# the values in oldcustomer database do not start with that
+# suffix. 
+addr_state = db.create_df("SELECT * from api_state;")
+
 
 for table in desired_tables:
 
     # Import the data
     df = db.import_all_data(table)
 
-    # Fix wrong labels
-    df.replace(wrong_labels, inplace=True)
+    # Upon inspection we see that there are three columns
+    # that contain wrong quantities.
+    # 1. loan_amnt, some rows are in string format with 'xx.k'
+    # 2. term, some rows are in years. We assume payments are
+    # monthly, so we can multiply that factor for 12 to obtain
+    # the number of payments in the loan.
+    # 3. issued has different datetime formats, one contain
+    # Days, others just Years and Months. Standarize them.
+    # 4. annual_inc has a range for some observations instead
+    # of a single value (e.g (72000.0, 80000.0]) 
+    # For those cases, compute the mean.
 
-    # Create dummies
-    
-# Merge dataframes
-    
-# Perform Nan substitution
+    # Fix wrong quantities and assing proper format to columns
+    df = fix_wrong_units(df, wanted_fields, fix_qties)
+    # Auxiliary column to debug
+    df['table'] = table
+    dfs.append(df)
+
+df = pd.concat(dfs)
+
+# Perform NaN treatment
+cols_with_nans = [col for col in df.columns if df[col].isnull().any()]
+print(f'The following columns have NaNs, {",".join(cols_with_nans)}')
+
+
+
+# After the NaN treatment, no NaNs should be present, assert it
+#if any(df.isnull()):
+#    raise ValueError(f'DataFrame from table {table} contains Nans, check!')
+
+# Create the dummy variables
+#df = create_dummies(df, 'verificationstatus')
+#df = create_dummies(df, 'homeownership')
 
 # Write to csv
-
-
-
-# Notes
-# 1. Newcustomers has a lot of columns with id. All those columns need
-# to be changed to their respective values
+#output_file = f'{os.getcwd()}/final_dataset.csv'
+#df.to_csv(output_file, index=False)
 
 # # PIPELINE
 # 1. Perform some explanatory analysis on the content of the tables
@@ -160,7 +362,7 @@ for table in desired_tables:
 # 5. Write the data into a csv file.
 
 # HOMEOWNERSHIP => integer, but we need the categorical values for the classy
-# PURPOSE => integer,
+# PURPOSE => integer, we do not need the name, just the purpose id
 # SUBGRADE => integer, so we need the id (we can skip querying it for the newcust)
 # VERIFICATIONSTATUS => integer, but we need the categoricals as well
-# STATE => integer
+# STATE => integer, we do not need the categorical values
