dc88746a5019a44b0f56d1f38763be77efde239c
Axel Borasino Marques <boma.axel@gmail.com>
2024-08-07 11:38:46 +0200

    Create package for plotting operations

---
+++ Library/plotting.py
@@ -0,0 +1,14 @@
+"""
+Package devoted to group multiple plotting functions
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def sns_lineplot(df:pd.DataFrame, x:str, y:str, gpby:str, title:str):
+    sns.set_theme()
+    fig, ax = plt.subplots(figsize=(20,5))
+    sns.lineplot(data=df, x=x, y=y, hue=gpby)
+    plt.title(title)
+    plt.show()
