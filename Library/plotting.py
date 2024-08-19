6b2fa1890be35bc313f4bcbd731d51a29c74c630
Axel Borasino Marques <boma.axel@gmail.com>
2024-08-19 09:27:41 +0200

    formatting commint for plotting.py
    - Add description for function

--- Library/plotting.py
+++ Library/plotting.py
@@ -7,6 +7,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def sns_lineplot(df:pd.DataFrame, x:str, y:str, gpby:str, title:str):
+    """
+    Given a dataframe df, it creates lineplots of x vs y, grouping
+    the df by gpby column
+    """
+    
     sns.set_theme()
     fig, ax = plt.subplots(figsize=(20,5))
     sns.lineplot(data=df, x=x, y=y, hue=gpby)
